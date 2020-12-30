3c080a75c6b0d18d05bc7f8d46661d9a3fe01119
marin <marinbosshincu@gmail.com>
2020-12-30 09:44:55 +0100

    Updated User Class (getters & setters)

--- src/User.java
+++ src/User.java
@@ -8,4 +8,68 @@ public class User {
     private String health_info;
     private String insurance_type;
     private String insurance_name;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getHealth_info() {
+        return health_info;
+    }
+
+    public void setHealth_info(String health_info) {
+        this.health_info = health_info;
+    }
+
+    public String getInsurance_type() {
+        return insurance_type;
+    }
+
+    public void setInsurance_type(String insurance_type) {
+        this.insurance_type = insurance_type;
+    }
+
+    public String getInsurance_name() {
+        return insurance_name;
+    }
+
+    public void setInsurance_name(String insurance_name) {
+        this.insurance_name = insurance_name;
+    }
 }
